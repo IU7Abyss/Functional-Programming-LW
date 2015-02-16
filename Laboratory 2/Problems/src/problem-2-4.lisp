cb5c7159e5bd29e2ea05d961548fac4a38849712
Bryan Took <BBQTD@users.noreply.github.com>
2015-02-17 00:40:46 +0300

    fix commentary (too long)

--- Laboratory 2/Problems/src/problem-2-4.lisp
+++ Laboratory 2/Problems/src/problem-2-4.lisp
@@ -1,5 +1,5 @@
 (defun trapezoid-area (parallel-side1 parallel-side2 height)
-	"Area of trapezoid is calculated by 2 parallel sides and height"
+	"Area of trapezoid by 2 parallel sides and height"
 	(*
 		0.5
 		(+ parallel-side1 parallel-side2)
