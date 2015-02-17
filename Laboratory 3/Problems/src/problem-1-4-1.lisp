6aa0d075e928c98b04e644f1b3892629fe33c260
Bryan Took <BBQTD@users.noreply.github.com>
2015-02-18 00:13:22 +0300

    fix style

--- Laboratory 3/Problems/src/problem-1-4-1.lisp
+++ Laboratory 3/Problems/src/problem-1-4-1.lisp
@@ -1,5 +1,4 @@
-(defun problem-1-4-1 (arg1 arg2 arg3)
-	"if arg1 between arg2, arg3 then true"
-	(equal 
-		arg1 
-		(second (sort (list arg1 arg2 arg3) #'<))))
+(defun is-first-between1 (arg1 arg2 arg3)
+    (equal arg1 
+           (second (sort (list arg1 arg2 arg3) 
+                         #'<))))
