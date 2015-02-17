b489e05305332370601569df6b1f6655d2ae1ab8
Bryan Took <BBQTD@users.noreply.github.com>
2015-02-17 22:49:48 +0300

    another implementation

--- Laboratory 3/Problems/src/problem-1-4-1.lisp
+++ Laboratory 3/Problems/src/problem-1-4-1.lisp
@@ -1,4 +1,5 @@
-(defun problem-1-4 (arg1 arg2 arg3)
-	(or
-		(and (< arg2 arg1) (< arg1 arg3))
-		(and (< arg3 arg1) (< arg1 arg2))))
+(defun problem-1-4-1 (arg1 arg2 arg3)
+	"if arg1 between arg2, arg3 then true"
+	(equal 
+		arg1 
+		(second (sort (list arg1 arg2 arg3) #'<))))
