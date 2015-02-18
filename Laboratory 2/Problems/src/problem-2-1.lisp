bbbd538f704bf89a7e67c3e5e723b435cf6dd2f7
Bryan Took <BBQTD@users.noreply.github.com>
2015-02-18 17:45:25 +0300

    fix style

--- Laboratory 2/Problems/src/problem-2-1.lisp
+++ Laboratory 2/Problems/src/problem-2-1.lisp
@@ -1,9 +1,8 @@
 (defun sqr (arg)
-	(* arg arg))
+    (* arg arg))
 
 (defun hypotenuse-rect-triangle (leg1 leg2)
-	"By Pythagorean theorem"
-	(sqrt 
-		(+ 
-			(sqr leg1) 
-			(sqr leg2))))
+    "By Pythagorean theorem"
+    (sqrt 
+        (+ (sqr leg1) 
+           (sqr leg2))))
