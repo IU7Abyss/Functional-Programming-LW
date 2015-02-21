9931a23835604ed09646847abcc16892b6503136
Bryan Took <BBQTD@users.noreply.github.com>
2015-02-21 14:46:57 +0300

    исходники для задания

---
+++ Laboratory 4/Problems/src/problem-3-1.lisp
@@ -0,0 +1,11 @@
+(defun problem-2-1 (arg)
+    (first (last arg)))
+
+(defun problem-2-1 (arg)
+    (car (last arg)))
+
+(defun problem-2-2 (arg)
+    (first (reverse arg)))
+
+(defun problem-2-3 (arg)
+    (car (reverse arg)))
---
+++ Laboratory 4/Problems/src/problem-3-2.lisp
@@ -0,0 +1,5 @@
+(defun problem-3-2-1 (arg)
+    (reverse (cdr (reverse arg))))
+
+(defun problem-3-2-2 (arg)
+    (butlast arg))
