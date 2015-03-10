3ad6fc9e7fab1ae0dcfb068c637677a5e5935e56
BBQTD <BBQTD@users.noreply.github.com>
2015-03-11 01:39:11 +0300

    fix

--- Laboratory 5/Problems/src/problem-2.lisp
+++ Laboratory 5/Problems/src/problem-2.lisp
@@ -1,2 +1,9 @@
-(defun set-equal (lst1 lst2)
-  (null (set-difference lst1 lst2)))
+(defun subset-mask (set1 set2)
+  (mapcar #'(lambda (x)
+              (let ((bits (mapcar #'(lambda (y) (and (equal x y) 1)) set2)))
+                (find-if #'(lambda (z) (numberp z)) bits)))
+          set1))
+
+(defun set-equalp (set1 set2)
+  (= (length (remove-if-not #'numberp (subset-mask set1 set2)))
+     (length set1)))
