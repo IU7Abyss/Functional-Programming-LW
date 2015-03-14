bbcaa3449393082c1a8bafd4b198686e5686b0ae
BBQTD <BBQTD@users.noreply.github.com>
2015-03-14 16:13:14 +0300

    fix bug in main function

--- Laboratory 5/Problems/src/problem-2.lisp
+++ Laboratory 5/Problems/src/problem-2.lisp
@@ -35,21 +35,17 @@
     (t (_normalize-set set))))
 
 (defun is-elements-in-set (set1 set2)
+  "Return mask of positions"
   (mapcar #'(lambda (_element) 
                       (if (in-list _element set2) 1 0))
           set1))
-
-(defun _set-equalp (set1 set2)
-  (let ((mask1 (is-elements-in-set set1 set2))
-        (c2 (length set2)))
-    (or (eql (c-count 1 mask1) c2)
-        0)))
 
 (defun _set-equalp (set1 set2)
   "Internal for set-equalp without validation"
   (let ((mask (mapcar #'(lambda (_element) 
                           (if (in-list _element set2) 1 0))
                       set1)))
+    (format t "~a, ~a" (c-count 1 mask) (length set2))
     (eql (c-count 1 mask) (length set2))))
 
 (defun set-equalp (set1 set2)
@@ -61,4 +57,4 @@
     (declare (fixnum length-set1 length-set2))
     (cond 
       ((/= length-set1 length-set2) nil)
-      (t (_set-equalp normal-set1 normal-set1)))))
+      (t (_set-equalp normal-set1 normal-set2)))))
