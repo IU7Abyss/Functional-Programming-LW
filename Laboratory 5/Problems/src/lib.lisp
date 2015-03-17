fca44a2daf9104e4df7ceff0967e7ef858f72d09
BBQTD <BBQTD@users.noreply.github.com>
2015-03-17 12:46:09 +0300

    Fix code style

--- Laboratory 5/Problems/src/lib.lisp
+++ Laboratory 5/Problems/src/lib.lisp
@@ -1,30 +1,30 @@
-(defun pos-element-in-list (element lst)
+(defun pos-element-in-list (element l)
   "Return mask where the element in the set"
   (mapcar #'(lambda (x)
               (if (equal element x) 1 0)) 
-          lst))
+          l))
 
-(defun c-count (element lst)
-  (let ((mask (pos-element-in-list element lst)))
+(defun c-count (element l)
+  (let ((mask (pos-element-in-list element l)))
     (reduce #'+ mask)))
 
-(defun in-list (element lst)
+(defun in-list (element l)
   "Return T if the element in the list"
-  (let* ((bits (pos-element-in-list element lst))
+  (let* ((bits (pos-element-in-list element l))
          (i (c-count 1 bits)))
     (declare (fixnum i))
     (< 0 i)))
 
-(defun not-in-list (element lst)
+(defun not-in-list (element l)
   "Return T if the element not in the list"
-  (not (in-list element lst)))
+  (not (in-list element l)))
 
 (defun symbol-or-listp (arg)
   (or (symbolp arg)
       (listp arg)))
 
-(defun c-list-length (lst)
-  (let ((bits (mapcar #'symbol-or-listp lst)))
+(defun c-list-length (l)
+  (let ((bits (mapcar #'symbol-or-listp l)))
     (c-count t bits)))
 
 (defun _c-reverse (l)
