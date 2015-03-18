a354c416e088eecbd0b5b2e02b372383d555a483
BBQTD <BBQTD@users.noreply.github.com>
2015-03-18 10:01:41 +0300

    Add Problem 6

---
+++ Laboratory 7/Problems/src/problem-6.lisp
@@ -0,0 +1,15 @@
+(defun it-union (x y)
+  (let ((result (copy-list x)))
+    (dolist (item y)
+      (if (not (member item result))
+        (push item result)))
+    result))
+
+(defun it-complement (x y)
+  (let ((result nil))
+    (dolist (item x result)
+      (when (not (member item y))
+        (push item result)))))
+
+(defun it-symmetric-difference (x y)
+  (it-union (it-complement x y) (it-complement y x)))
