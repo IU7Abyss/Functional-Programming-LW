0d586af87fd820626f89c75c96ff56bd17b82554
BBQTD <BBQTD@users.noreply.github.com>
2015-03-19 20:36:49 +0300

    Fix code

--- Laboratory 6/Problems/src/problem-4.lisp
+++ Laboratory 6/Problems/src/problem-4.lisp
@@ -1,13 +1,14 @@
 (defun between (lst left right)
+  "Return new list with numbers between left and right"
   (let ((item (first lst))
         (tail (rest lst)))
     (cond ((null item) nil)
           ((numberp item)
-           (cond ((<= left item right) ; to another function
+           (cond ((<= left item right)
                   (cons item
                         (between tail left right)))
                  (t
                   (between tail left right))))
           ((listp item)
-           (append (between item left right)
-                   (between tail left right))))))
+           (nconc (between item left right)
+                  (between tail left right))))))
