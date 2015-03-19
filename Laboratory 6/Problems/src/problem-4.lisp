5e2c8f3f1777990169f915530815dcee1cacbda7
BBQTD <BBQTD@users.noreply.github.com>
2015-03-19 15:57:14 +0300

    Fix Problem 4

--- Laboratory 6/Problems/src/problem-4.lisp
+++ Laboratory 6/Problems/src/problem-4.lisp
@@ -1,6 +1,13 @@
-(defun f4 (lst left right) (
-  remove-if #'(lambda (x) (
-    or (<= x left) (>= x right) 
-  )) 
-  lst     
-))
+(defun between (lst left right)
+  (let ((item (first lst))
+        (tail (rest lst)))
+    (cond ((null item) nil)
+          ((numberp item)
+           (cond ((<= left item right) ; to another function
+                  (cons item
+                        (between tail left right)))
+                 (t
+                  (between tail left right))))
+          ((listp item)
+           (append (between item left right)
+                   (between tail left right))))))
