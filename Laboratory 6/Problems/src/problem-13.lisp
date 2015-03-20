7cc2c37d5b13127186d3de4e2f6e6b84a0ab06de
BBQTD <BBQTD@users.noreply.github.com>
2015-03-20 23:27:17 +0300

    Fix Problem 13

--- Laboratory 6/Problems/src/problem-13.lisp
+++ Laboratory 6/Problems/src/problem-13.lisp
@@ -1,8 +1,9 @@
-(defun f13 (lst &optional (rez nil)) (
-  cond 
-    ((null lst) rez) 
-    ((oddp (car lst)) (f13 (cdr lst) (car lst))) 
-    (T 
-      (f13 (cdr lst) rez)
-    ) 
-))
+(defun first-odd (lst)
+  (let ((item (first lst))
+        (tail (rest lst)))
+    (cond ((null lst) nil)
+          ((oddp item) item)
+          (t (first-odd tail)))))
+
+(defun last-odd (lst)
+  (first-odd (reverse lst)))
