e2ed8348695b625faa771323c62875d9711207bc
BBQTD <BBQTD@users.noreply.github.com>
2015-03-20 23:44:56 +0300

    Fix Problem 14

--- Laboratory 6/Problems/src/problem-14.lisp
+++ Laboratory 6/Problems/src/problem-14.lisp
@@ -1,10 +1,10 @@
-(defun f14 (lst) (
-  cond
-    ((null lst) nil)
-    (T
-      (cons
-        (* (first lst) (first lst))
-        (f14 (rest lst))
-      )
-    )
-))
+(defun square (lst)
+  (let ((item (first lst))
+        (tail (rest lst)))
+    (cond ((null lst) nil)
+          ((consp item)
+           (cons (square item)
+                 (square tail)))
+           (t 
+            (cons (* item item)
+                  (square tail))))))
